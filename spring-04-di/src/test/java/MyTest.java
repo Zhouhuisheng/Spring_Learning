d966be4f305124788c0d77aff785fcd194bbebc7
周辉生 <391030990@qq.com>
2023-03-21 22:17:25 +0800

    p命名注入和c命名注入，添加了junit测试

--- spring-04-di/src/test/java/MyTest.java
+++ spring-04-di/src/test/java/MyTest.java
@@ -1,4 +1,6 @@
 import com.kuang.pojo.Student;
+import com.kuang.pojo.User;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -13,5 +15,23 @@ public class MyTest {
 
         // 测试
         System.out.println(student.toString());
+    }
+
+    @Test
+    public void test2() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("userbeans.xml");
+        User user = (User) context.getBean("user");
+
+        // 测试
+        System.out.println(user.toString());
+    }
+
+    @Test
+    public void test3() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("userbeans.xml");
+        User user = (User) context.getBean("user2");
+
+        // 测试
+        System.out.println(user.toString());
     }
 }
